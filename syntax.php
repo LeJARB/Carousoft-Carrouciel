a50074de2c0d48fee0162021e39509d087c6b08d
Le JARB <75838352+LeJARB@users.noreply.github.com>
2025-12-19 13:37:21 +0100

    Adding Glowing effect in Thumbnails & Infobox for MSIE 5.5

--- syntax.php
+++ syntax.php
@@ -129,11 +129,11 @@
 					{
 						if(count($matches) == 1)
 						{
-							$thumbnails .= "\r\n<".($isie4?"span id=\"thumblistmobile\"":"div align=\"center\"")." class=\"thumblistmobile\">\r\n".($isie4||$isie5?"<div class=\"msie45glow\">":"")."<table cellpadding=\"20px\" cellspacing=\"10px\" width=\"100%\"".($isie?" id=\"thumblisttab\"":"")." class=\"thumblisttab\">\r\n<tr><td align=\"center\" bgcolor=\"#242424\" class=\"thumbnails\"><font face=\"Arial,Helvetica,sans-serif\" size=\"4\">".$matches[0]."</font></td></tr>\r\n</table>".($isie4||$isie5?"</div>":"")."</".($isie4?"span":"div").">";
+							$thumbnails .= "\r\n<".($isie4?"span id=\"thumblistmobile\"":"div align=\"center\"")." class=\"thumblistmobile\">\r\n".($isie4||$isie5||$isie55?"<div class=\"msie45glow\">":"")."<table cellpadding=\"20px\" cellspacing=\"10px\" width=\"100%\"".($isie?" id=\"thumblisttab\"":"")." class=\"thumblisttab\">\r\n<tr><td align=\"center\" bgcolor=\"#242424\" class=\"thumbnails\"><font face=\"Arial,Helvetica,sans-serif\" size=\"4\">".$matches[0]."</font></td></tr>\r\n</table>".($isie4||$isie5||$isie55?"</div>":"")."</".($isie4?"span":"div").">";
 						}
 						else
 						{
-							$thumbnails .= "\r\n<".($isie4?"span id=\"thumblistmobile\"":"div align=\"center\"")." class=\"thumblist\"><center>\r\n".($isie4||$isie5?"<div class=\"msie45glow\">":"")."<table cellpadding=\"10px\" cellspacing=\"0px\" width=\"120px\">\r\n<tr><td align=\"center\" bgcolor=\"#242424\" class=\"thumbnails\"><a href=\"http$https://$host/$lang/".$matches[1]."\">".$matches[2]."</a></td></tr></table>".($isie4||$isie5?"</div>":"")."\r\n<table cellpadding=\"10px\" cellspacing=\"0px\" width=\"120px\"><tr><td align=\"center\"><font face=\"Arial,Helvetica,sans-serif\" size=\"4\"><a href=\"http$https://$host/$lang/".$matches[1]."\">".$matches[0]."</a></font></td></tr>\r\n</table>\r\n</center></".($isie4?"span":"div").">";
+							$thumbnails .= "\r\n<".($isie4?"span id=\"thumblistmobile\"":"div align=\"center\"")." class=\"thumblist\"><center>\r\n".($isie4||$isie5||$isie55?"<div class=\"msie45glow\">":"")."<table cellpadding=\"10px\" cellspacing=\"0px\" width=\"120px\">\r\n<tr><td align=\"center\" bgcolor=\"#242424\" class=\"thumbnails\"><a href=\"http$https://$host/$lang/".$matches[1]."\">".$matches[2]."</a></td></tr></table>".($isie4||$isie5||$isie55?"</div>":"")."\r\n<table cellpadding=\"10px\" cellspacing=\"0px\" width=\"120px\"><tr><td align=\"center\"><font face=\"Arial,Helvetica,sans-serif\" size=\"4\"><a href=\"http$https://$host/$lang/".$matches[1]."\">".$matches[0]."</a></font></td></tr>\r\n</table>\r\n</center></".($isie4?"span":"div").">";
 						}
 						continue;
 					}
@@ -147,11 +147,11 @@
 				{
 					if(count($matches) == 1)
 					{
-						$thumbnails = "<center><table cellpadding=\"0px\" cellspacing=\"0px\" width=\"100%\"><tr><td align=\"center\">\r\n<".($isie4?"span id=\"thumblistmobile\"":"div align=\"center\"")." class=\"thumblistmobile\">\r\n".($isie4||$isie5?"<div class=\"msie45glow\">":"")."<table cellpadding=\"20px\" cellspacing=\"10px\" width=\"100%\"".($isie?" id=\"thumblisttab\"":"")." class=\"thumblisttab\">\r\n<tr><td align=\"center\" bgcolor=\"#242424\" class=\"thumbnails\"><font face=\"Arial,Helvetica,sans-serif\" size=\"4\">".$matches[0]."</font></td></tr>\r\n</table>".($isie4||$isie5?"</div>":"")."</".($isie4?"span":"div").">";
+						$thumbnails = "<center><table cellpadding=\"0px\" cellspacing=\"0px\" width=\"100%\"><tr><td align=\"center\">\r\n<".($isie4?"span id=\"thumblistmobile\"":"div align=\"center\"")." class=\"thumblistmobile\">\r\n".($isie4||$isie5||$isie55?"<div class=\"msie45glow\">":"")."<table cellpadding=\"20px\" cellspacing=\"10px\" width=\"100%\"".($isie?" id=\"thumblisttab\"":"")." class=\"thumblisttab\">\r\n<tr><td align=\"center\" bgcolor=\"#242424\" class=\"thumbnails\"><font face=\"Arial,Helvetica,sans-serif\" size=\"4\">".$matches[0]."</font></td></tr>\r\n</table>".($isie4||$isie5||$isie55?"</div>":"")."</".($isie4?"span":"div").">";
 					}
 					else
 					{
-						$thumbnails = "<center><table cellpadding=\"0px\" cellspacing=\"0px\" width=\"100%\"><tr><td align=\"center\">\r\n<".($isie4?"span id=\"thumblistmobile\"":"div align=\"center\"")." class=\"thumblist\"><center>\r\n".($isie4||$isie5?"<div class=\"msie45glow\">":"")."<table cellpadding=\"10px\" cellspacing=\"0px\" width=\"120px\">\r\n<tr><td align=\"center\" bgcolor=\"#242424\" class=\"thumbnails\"><a href=\"http$https://$host/$lang/".$matches[1]."\">".$matches[2]."</a></td></tr></table>".($isie4||$isie5?"</div>":"")."\r\n<table cellpadding=\"10px\" cellspacing=\"0px\" width=\"120px\"><tr><td align=\"center\"><font face=\"Arial,Helvetica,sans-serif\" size=\"4\"><a href=\"http$https://$host/$lang/".$matches[1]."\">".$matches[0]."</a></font></td></tr>\r\n</table>\r\n</center></".($isie4?"span":"div").">";
+						$thumbnails = "<center><table cellpadding=\"0px\" cellspacing=\"0px\" width=\"100%\"><tr><td align=\"center\">\r\n<".($isie4?"span id=\"thumblistmobile\"":"div align=\"center\"")." class=\"thumblist\"><center>\r\n".($isie4||$isie5||$isie55?"<div class=\"msie45glow\">":"")."<table cellpadding=\"10px\" cellspacing=\"0px\" width=\"120px\">\r\n<tr><td align=\"center\" bgcolor=\"#242424\" class=\"thumbnails\"><a href=\"http$https://$host/$lang/".$matches[1]."\">".$matches[2]."</a></td></tr></table>".($isie4||$isie5||$isie55?"</div>":"")."\r\n<table cellpadding=\"10px\" cellspacing=\"0px\" width=\"120px\"><tr><td align=\"center\"><font face=\"Arial,Helvetica,sans-serif\" size=\"4\"><a href=\"http$https://$host/$lang/".$matches[1]."\">".$matches[0]."</a></font></td></tr>\r\n</table>\r\n</center></".($isie4?"span":"div").">";
 					}
 					continue;
 				}
@@ -177,7 +177,7 @@
 					}
 					else
 					{
-						$line = $infobox."".($isie4||$isie5?"</div>":"")."\r\n</center>";
+						$line = $infobox."".($isie4||$isie5||$isie55?"</div>":"")."\r\n</center>";
 						unset($infobox);
 					}
 				}
@@ -185,11 +185,11 @@
 				{
 					if(count($matches) > 1) 
 					{
-						$infobox = "<center>\r\n".($isie4||$isie5?"<div class=\"msie45glow\">":"")."<div class=\"infoboxmobile\" id=\"infobox\">\r\n<table class=\"infobox\" cellpadding=\"5px\" cellspacing=\"0px\" width=\"280px\" bgcolor=\"#242424\">\r\n<tr><td><font face=\"Arial,Helvetica,sans-serif\" size=\"4\">".$matches[0]."</font></td><td><font face=\"Arial,Helvetica,sans-serif\" size=\"4\">".$matches[1]."</font></td></tr>\r\n</table></div>";
+						$infobox = "<center>\r\n".($isie4||$isie5||$isie55?"<div class=\"msie45glow\">":"")."<div class=\"infoboxmobile\" id=\"infobox\">\r\n<table class=\"infobox\" cellpadding=\"5px\" cellspacing=\"0px\" width=\"280px\" bgcolor=\"#242424\">\r\n<tr><td><font face=\"Arial,Helvetica,sans-serif\" size=\"4\">".$matches[0]."</font></td><td><font face=\"Arial,Helvetica,sans-serif\" size=\"4\">".$matches[1]."</font></td></tr>\r\n</table></div>";
 					}
 					else
 					{
-						$infobox = "<center>\r\n".($isie4||$isie5?"<div class=\"msie45glow\">":"")."<div class=\"infoboxmobile\" id=\"infobox\">\r\n<table class=\"infobox\" cellpadding=\"5px\" cellspacing=\"0px\" width=\"280px\" bgcolor=\"#242424\">\r\n<tr><td align=\"center\" colspan=\"2\"><font face=\"Arial,Helvetica,sans-serif\" size=\"4\">".$matches[0]."</font></td></tr>\r\n</table></div>";
+						$infobox = "<center>\r\n".($isie4||$isie5||$isie55?"<div class=\"msie45glow\">":"")."<div class=\"infoboxmobile\" id=\"infobox\">\r\n<table class=\"infobox\" cellpadding=\"5px\" cellspacing=\"0px\" width=\"280px\" bgcolor=\"#242424\">\r\n<tr><td align=\"center\" colspan=\"2\"><font face=\"Arial,Helvetica,sans-serif\" size=\"4\">".$matches[0]."</font></td></tr>\r\n</table></div>";
 					}
 					continue;
 				}

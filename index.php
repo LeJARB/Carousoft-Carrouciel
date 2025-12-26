b76a28f689b4b74d1f4ca78f825c56ca9b9142ac
Le JARB <75838352+LeJARB@users.noreply.github.com>
2025-12-26 01:26:58 +0100

    Correct a bug with the redirection URL

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 
 	// Obtain URL/Obtention de l'URL
 	if(isset($_SERVER['REDIRECT_URL']))
+	{
 		$_SERVER['REDIRECT_URL'] = str_replace(" ",".",$_SERVER['REDIRECT_URL']);
 		$url = substr($_SERVER['REDIRECT_URL'],4);
+	}
 
 	// Call to/Appel à core.php
 	require('core.php');
